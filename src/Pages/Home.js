d979904b1cff3691d385bfbae8ddccdb1632d1e2
jpm <jpmckenna90@gmail.com>
2020-07-19 15:15:55 -0400

    Started building out more of homepage, fiddling with text animations

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,28 @@
 import React from "react";
-
+import { Container, Div, Row, Col } from "react-bootstrap";
 
 function Home() {
   return (
-    <video autoPlay muted loop id="mainVideo">
-      <source
-        src={require("../Assets/Videos/demo.mp4")}
-        type="video/ogg"
-      ></source>
-    </video>
+    <>
+      <video autoPlay muted loop id="mainVideo">
+        <source
+          src={require("../Assets/Videos/demo.mp4")}
+          type="video/ogg"
+        ></source>
+      </video>
+      <Container>
+        <Row className="my-4">
+          <Col className="text-center animate__animated animate__fadeIn">
+            <h1>Attention is hard to capture.</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="text-center">
+            <h3>Brands that capture attention will:</h3>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
