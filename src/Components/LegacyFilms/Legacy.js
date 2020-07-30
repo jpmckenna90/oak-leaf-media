783b865225b8d52291a12cb1df7b4ba56d384161
jpm <jpmckenna90@gmail.com>
2020-07-29 22:23:08 -0400

    Some more components, working on IntersectionObserver to fade some things in

--- src/Components/LegacyFilms/Legacy.js
+++ src/Components/LegacyFilms/Legacy.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "./style.css";
 
 function Legacy() {
+  const [isVisible, setVisible] = useState(true);
+  const domRef = React.useRef();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => entry.isIntersecting && setVisible(true));
+    });
+
+    observer.observe(domRef.current);
+
+    // return () => observer.unobserve(domRef.current);
+  }, []);
+
   return (
-    <Container style={{ marginTop: "20%" }} id="legacy">
+    <Container
+      ref={domRef}
+      className={`fade-in-section  ${isVisible ? "is-visible" : ""}`}
+      style={{ marginTop: "20%" }}
+      id="legacy"
+    >
       <Row>
         <Col></Col>
         <Col>
@@ -14,24 +32,6 @@ function Legacy() {
         </Col>
       </Row>
       <Row>
-        <Col>
-          <p>
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-            sed quia non numquam eius modi tempora incidunt ut labore et dolore
-            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-            aliquid ex ea commodi consequatur? Quis autem vel eum iure
-            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
-            pariatur?"
-          </p>
-        </Col>
         <Col>
           <div style={{ padding: "56.25% 0 0 0" }}>
             <iframe
@@ -50,6 +50,24 @@ function Legacy() {
             ></iframe>
           </div>
           <script src="https://player.vimeo.com/api/player.js"></script>
+        </Col>
+        <Col>
+          <p>
+            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+            sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
+            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+            aliquid ex ea commodi consequatur? Quis autem vel eum iure
+            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+            pariatur?"
+          </p>
         </Col>
       </Row>
     </Container>
